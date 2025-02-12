64907f4b5ebd1df8fe9a55ab92c379820fb1d50e
Michael Monaghan <Nostromos@users.noreply.github.com>
2025-02-12 14:18:25 -0500

    Fix: Update component style naming to standardize format

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,20 +1,21 @@
 import clsx from "clsx"
-import styles from "./CardStyles.module.css"
+import styles from "./ProjectCardStyles.module.css"
 import Placeholder from "@site/static/img/placeholder.svg"
 
 interface ProjectCardProps {
   image: string
   tags: string[]
   name: string
-  status: "completed" | "in progress" | "abandoned"
+  status: "Completed" | "In Progress" | "Abandoned" | "Ideation"
   description: string
 }
 
 export function ProjectCard({ image, tags, name, status, description }: ProjectCardProps) {
   const statusClassName = clsx(styles.cardStatus, {
-    [styles["cardStatus--completed"]]: status === "completed",
-    [styles["cardStatus--inProgress"]]: status === "in progress",
-    [styles["cardStatus--abandoned"]]: status === "abandoned",
+    [styles["cardStatus--completed"]]: status === "Completed",
+    [styles["cardStatus--inProgress"]]: status === "In Progress",
+    [styles["cardStatus--abandoned"]]: status === "Abandoned",
+    [styles["cardStatus--ideation"]]: status === "Ideation",
   })
 
   return (
